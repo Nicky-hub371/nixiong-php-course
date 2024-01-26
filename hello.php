ee1e254e7bf6895eec878f31882e4f8f254db49e
Ni.Xiong <xiong3ni@hotmail.com>
2024-01-26 10:58:37 +0200

    Initial commit

---
+++ hello.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Test</title>
+</head>
+<body>
+<!--this is a comment outside the php but in the html-->
+
+<?php
+// this is printing hello world
+/* multiple
+lines
+comments
+*/
+echo"<h1>Hello World</h1>";
+
+?>
+
+
+
+
+
+
+
+
+</body>
+</html>
+
+
+
