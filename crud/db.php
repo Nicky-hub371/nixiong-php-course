3c8d90ad8039bb9e0d45c914b0a301f02362ba4d
Ni.Xiong <xiong3ni@hotmail.com>
2024-02-05 11:37:13 +0200

    update database connections

---
+++ crud/db.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "web-dev-env-main-db-1";
+$username = "app1";
+$password = "password";
+$dbname = "app1";
+
+
+// create databse connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// check connection
+if ($conn -> connect_error){
+    die("Connection Failed:". $conn -> connect_error);
+}
+
+?>
---
+++ crud/process.php
@@ -0,0 +1,27 @@
+<?php
+// what to do with the data
+if(isset($_POST['submit'])){
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $city = $_POST['city'];
+    $groupid = $_POST['groupid'];
+
+    //connect to the databse server
+    include 'db.php';
+
+    // write sql statement to insert data
+    $sql = "insert into studentsinfo (first_name, last_name, city, groupId)
+    values('$fname', '$lname', '$city', '$groupid')";
+
+if($conn -> query($sql) === TRUE){
+    echo "Your data was recorded";
+}else{
+    echo "Error: ".$sql."<br>".$conn->error;
+}
+
+//close the databse connection
+$conn -> close();
+
+}
+
+?>
