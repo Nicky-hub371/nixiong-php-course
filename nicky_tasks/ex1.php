3568d97b8994527d27031ca804b6f829852f9f56
Ni.Xiong <xiong3ni@hotmail.com>
2024-02-09 08:58:46 +0200

    update ex1.php for image width

--- nicky_tasks/ex1.php
+++ nicky_tasks/ex1.php
@@ -46,7 +46,7 @@ echo"<h4>$title</h4>";
     </table>
 <h3>3.4 Take a screenshot that confirms your development environment setup 
     and include it as an image in the "ex1.php" file.</h3>
-    <img src ="images/24.png" alt="web development environment">
+    <img src ="images/24.png" alt="web development environment" style="width: 800px;>
 
 <h3>3.5 Commit all the changes you've made and push them to your GitHub repository 
     to complete the task.</h3>
